98b8ce9180897eabf083503ddd46adeb09ff300f
nooboftime <tamara_walling@yahoo.com>
2024-07-11 10:27:38 -0500

    def get employee todo list

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,3 +3,15 @@
 
 import requests
 import sys
+
+
+def get_employee_todo_list(employee_id):
+    """Fetch and display TODO list progress for a given employee ID."""
+    url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    user_response = requests.get(url)
+    if user_response.status_code != 200:
+        print(f"User with id {employee_id} not found")
+        return
+
+user = user_response.json()
+name = user['name']
